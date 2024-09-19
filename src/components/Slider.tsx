1d6bbe19bb294245349789afc0de98586c01fa26
Rajat Raj <rajatrajemails@gmail.com>
2024-09-19 19:57:41 +0530

    Updated Slider.tsx

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import Link from "next/link";
 
 const data = [
   {
@@ -38,7 +39,9 @@ const Slider = () => {
         <h1 className="text-5xl text-center uppercase p-4 md:text-6xl x:text-7xl">
           {data[currentSlide].title}
         </h1>
-        <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
+        <button className="bg-red-500 text-white py-4 px-8">
+          <Link href="/menu">Order Now</Link>
+        </button>
       </div>
       {/* Image Container */}
       <div className="w-full relative flex-1">
